5d2655a2d2a28192d74040f0b8f5f8bc4206c01b
SaiTeja-002 <teja00219@gmail.com>
2022-06-09 22:59:52 +0530

    Added SpotLights and day and night option

---
+++ VR Game/Assets/Scripts/DirectionalLightMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DirectionalLightMotion : MonoBehaviour
+{
+
+    public float rotationalSpeed = 7.0f;    //The speed at which the DirectionalLight rotates
+    
+    public static Action LightOffAction = null; //Action used to specify that the DirectionalLight is not focused
+    public static Action LightOnAction = null;  //Action used to specify that the DirectionalLight is focused
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime*rotationalSpeed, 0); //Rotating the DirectionalLight continuously
+        
+        float directionalLightY = transform.rotation.eulerAngles.y; //Angle of the DirectionalLight made with the Y-axis
+
+        /* Angles at which the DirectionalLight is not foused */
+        if(directionalLightY > 100.0f && directionalLightY < 260.0f)
+        {
+            if(LightOffAction != null)
+                LightOffAction();
+        }
+
+        else    //Corresponds to the angles at which the DirectionalLight is focused
+        {
+            if(LightOnAction != null)
+                LightOnAction();
+        }
+    }
+}
---
+++ VR Game/Assets/Scripts/OptionSpotLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class OptionSpotLight : MonoBehaviour
+{
+    public GameObject spotLight;    //SpotLight object
+
+    /* Subscribing to the event OnEnable */
+    void OnEnable()
+    {
+        DirectionalLightMotion.LightOnAction += SpotLightOff;
+        DirectionalLightMotion.LightOffAction += SpotLightOn;
+    }
+
+    /* Unsubscribing to the event OnDisable */
+    void OnDisable()
+    {
+        DirectionalLightMotion.LightOnAction -= SpotLightOff;
+        DirectionalLightMotion.LightOffAction -= SpotLightOn;
+    }
+
+    /* Function used to set the SpotLight active */
+    void SpotLightOn()
+    {
+        spotLight.SetActive(true);
+    }
+
+    /* Function used to set the SpotLight inactive */
+    void SpotLightOff()
+    {
+        spotLight.SetActive(false);
+    }
+}
